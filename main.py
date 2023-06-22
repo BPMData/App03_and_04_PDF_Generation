7fdc1572b76c6933041fba52ae211b49acfb0d6b
BPMData <bryan.patrick.a.murphy@gmail.com>
2023-06-22 18:34:55 -0400

    Committing this in case I break my code by trying to move the minion imports to another file

--- main.py
+++ main.py
@@ -1,23 +1,38 @@
 from fpdf import FPDF
-# import pathlib
-# minionpath = pathlib.PureWindowsPath("C:\Users\corma\AppData\Local\Microsoft\Windows\Fonts\MinionPro-Regular.otf")
-# print(minionpath)
 
 pdf = FPDF(orientation="portrait", unit="mm", format="A4")
 
+print(pdf)
 
-print(type(pdf))
-
-print(dir(pdf))
-
-
-
-pdf.add_font(family="MinionReg", fname="C:\Fonts\Minion\MinionPro-Regular.ttf",
+pdf.add_font(family="Minion", fname="C:\Fonts\Minion\TTF\MinionProReg.ttf",
             uni=True)
-pdf.set_font(family="MinionReg", size=12)
+
+pdf.add_font(family="MinionItal", fname="C:\Fonts\Minion\TTF\MinionProItal.ttf",
+            uni=True)
+
+pdf.add_font(family="MinionBold", fname="C:\Fonts\Minion\TTF\MinionProBold.ttf",
+            uni=True)
+
+pdf.add_font(family="MinionBoldItal", fname="C:\Fonts\Minion\TTF\MinionProBoldItal.ttf",
+            uni=True)
+
+pdf.add_font(family="MinionImpact", fname="C:\Fonts\Minion\TTF\MinionProBigBold.ttf",
+            uni=True)
+
+pdf.add_font(family="MinionImpactItal", fname="C:\Fonts\Minion\TTF\MinionProBigBoldItal.ttf",
+            uni=True)
+
+pdf.set_font(family="MinionImpactItal", style="U", size=12)
+
+# A problem with using Minion is you'll have to specify a different file type directly for italics, underlined, bold, etc...
+# Actually, underlined is okay.
+
+
+# Sad to say but I might just go with Courier or Times...
+# BUT NOT RIGHT NOW!
 
 pdf.add_page()
 
-pdf.cell(w=0,h=12,txt="Hello There", align="L", ln=1, border=1)
+pdf.cell(w=0, h=12, txt="Hello There", align="L", ln=1, border=1)
 
-pdf.output(("output.pdf"))
+# pdf.output(("outputMinionBOLDU.pdf"))
