4661a7adc1afe089c0322a722932cd9d4ca51c1e
BPMData <bryan.patrick.a.murphy@gmail.com>
2023-06-23 16:31:59 -0400

    Okay, I'm done with these PDF generators, now onto Module 3, and a new project: Creating a positive news aggregator using APIs! Exciting!

--- debugger_test.py
+++ debugger_test.py
@@ -1,11 +1,31 @@
 # from https://www.udemy.com/course/the-python-mega-course/learn/lecture/34604378#search
 
-text = "I love sun"
-word = "love"
+# text = "I love sun"
+# word = "love"
+#
+# words = text.split(" ")
+# count = text.count(word)
+#
+# frequency = count / len(words) * 100
+#
+# print(frequency)
 
-words = text.sleep(" ")
-count = text.count("word")
+message = "Hello"
 
-frequency = count / len(words) * 100
+import glob
 
+def get_frequency(text, word):
+    words = text.split(" ")
+    count = text.count(word)
+    glob.glob("*")
+    frequency = count / len(words) * 100
+    return frequency
+
+frequency = get_frequency("I love sun", "love")
 print(frequency)
+
+if frequency > 5:
+    print("High frequency")
+else:
+    print("Low frequency")
+print(message)
