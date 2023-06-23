48dfc7252c911b1e1ef881d309b0134d3b791754
BPMData <bryan.patrick.a.murphy@gmail.com>
2023-06-23 16:09:46 -0400

    Learning how to use a debugger from https://www.udemy.com/course/the-python-mega-course/learn/lecture/34604378#search.
    Thus, there are intentional errors in this code.

---
+++ debugger_test.py
@@ -0,0 +1,11 @@
+# from https://www.udemy.com/course/the-python-mega-course/learn/lecture/34604378#search
+
+text = "I love sun"
+word = "love"
+
+words = text.sleep(" ")
+count = text.count("word")
+
+frequency = count / len(words) * 100
+
+print(frequency)
