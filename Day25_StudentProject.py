655bb88014edc2de86e513de017149beaae8e26e
BPMData <bryan.patrick.a.murphy@gmail.com>
2023-06-22 21:35:29 -0400

    With a little help from ChatGPT, I'm nearly done with both today and tomorrow's Student project, but I'm about to attempt something with a regular expression, which I've NEVER done before, so I'm trusting ChatGPT entirely on this, and thus I'm making a commit in case this breaks my code!

---
+++ Day25_StudentProject.py
@@ -0,0 +1,29 @@
+from fpdf import FPDF
+from funcs import get_minion
+import pandas as pd
+import glob
+from pathlib import Path
+import os
+
+pdf = FPDF(orientation="p", unit="mm", format="A4")
+get_minion(pdf)
+
+filepaths = glob.glob("invoice_data/txts/*txt")
+
+for filepath in filepaths:
+    with open(filepath, "r") as file:
+        lines = file.readlines()
+        text="".join(lines)
+    pathname = Path(filepath).stem
+    pdf.add_page()
+    # Generate page header/title
+    pdf.set_font(family="MinionImpactItal", size=36)
+    pdf.cell(w=0, h=18, txt=f"{pathname.title()}", ln=2, align="C",
+             fill=False, border=True)
+    # Generate body text
+    pdf.set_font(family="Minion", size=14)
+    pdf.multi_cell(w=0, h=12, txt=f"{text}", border=0)
+
+if not os.path.exists("invoice_data/txts/pdfs"):
+        os.mkdir("invoice_data/txts/pdfs")
+pdf.output(f"invoice_data/txts/pdfs/concat2.pdf")
--- invoice_main.py
+++ invoice_main.py
@@ -1,3 +1,32 @@
 from fpdf import FPDF
 from funcs import get_minion
 import pandas as pd
+import glob
+from pathlib import Path
+import os
+
+pdf = FPDF(orientation="p", unit="mm", format="A4")
+get_minion(pdf)
+
+filepaths = glob.glob("invoice_data/*xlsx")
+
+for filepath in filepaths:
+    df = pd.read_excel(filepath, sheet_name="Sheet 1")
+    print(df)
+    pathname = Path(filepath).stem
+    pdf.add_page()
+    pdf.set_font(family="MinionBold", size=16)
+    pdf.cell(w=50, h=8, txt="Invoice #" + pathname[0:5], ln=1)
+    pdf.cell(w=50, h=8, txt="Date " + pathname[6:])
+    # could also use invoice_nr = filename.split("-")[0], which is technically more scalable.
+    if not os.path.exists("invoice_pdfs"):
+        os.mkdir("invoice_pdfs")
+    pdf.output(f"invoice_pdfs/{pathname}.pdf")
+
+
+testpath = Path(filepaths[0]).stem
+print(testpath[0:5])
+
+
+
+
